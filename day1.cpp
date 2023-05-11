6a99515c6a57d1aedd7d2b1eacee3b572af6f973
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-05-11 23:07:28 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,18 @@
+//You are given an array of n strings strs, all of the same length.
+//The strings can be arranged such that there is one on each line, making a grid.
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int count=0;
+        for(int i=0;i<strs[0].size();i++){
+            for(int j=0;j<strs.size()-1;j++){
+                if(strs[j][i]>strs[j+1][i]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
