bd380809ff81f80c77211d698852049ce2d9dcb8
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-05-13 16:41:07 +0530

    day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -1,1 +1,16 @@
+//Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+//Return the running sum of nums.
 
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+     int sum=0;
+     vector<int> ans;
+     for(int i=0;i<nums.size();i++)
+     {
+         sum=sum+nums[i];
+         ans.push_back(sum);
+     }   
+     return ans;
+    }
+};
