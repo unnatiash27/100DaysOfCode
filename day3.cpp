6b410944e6a058cd651c3c69c61f5fb241994052
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-05-13 17:15:38 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -38,3 +38,22 @@ public:
         return (ans[ans.size() - 1]);
     }
 };
+
+// Given an array of integers nums, return the number of good pairs.A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
