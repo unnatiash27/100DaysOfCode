12152df246ae7fe03428f8fddbd69a4ed3ff8d72
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-05-13 17:06:17 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -14,3 +14,27 @@ public:
      return ans;
     }
 };
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+
+        vector<int> ans;
+
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < accounts[0].size(); j++)
+            {
+                sum = sum + accounts[i][j];
+            }
+            ans.push_back(sum);
+            sort(ans.begin(), ans.end());
+        }
+        return (ans[ans.size() - 1]);
+    }
+};
