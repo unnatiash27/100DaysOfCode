c853d054c5636e9cf32a817f568f79238179965e
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-05-15 20:45:51 +0530

    Update day5.cpp

--- day5.cpp
+++ day5.cpp
@@ -20,3 +20,50 @@ public:
         return s;
     }
 };
+
+
+// You own a Goal Parser that can interpret a string command.
+// The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
+// The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
+// The interpreted strings are then concatenated in the original order.
+// Given the string command, return the Goal Parser's interpretation of command.
+
+class Solution
+{
+public:
+    string interpret(string command)
+    {
+        string s = "";
+        for (int i = 0; i < command.size();)
+        {
+            if (command[i] == 'G')
+            {
+                s += 'G';
+                i++;
+            }
+            else if (command[i] == '(' && command[i + 1] == ')')
+            {
+                s += 'o';
+                i += 2;
+            }
+            else
+            {
+                s += "al";
+                i += 4;
+            }
+        }
+        return s;
+    }
+};
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+class Solution
+{
+public:
+    string toLowerCase(string s)
+    {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        return s;
+    }
+};
