6e6fa453027a37059e26ed757b29676d0f045978
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-01 19:51:14 +0530

    Create day22.cpp

---
+++ day22.cpp
@@ -0,0 +1,19 @@
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+class Solution
+{
+public:
+    int subtractProductAndSum(int n)
+    {
+        int s = 0, p = 1;
+        while (n != 0)
+        {
+            int d = n % 10;
+            s = s + d;
+            p = p * d;
+            n = n / 10;
+        }
+        int ans = p - s;
+        return ans;
+    }
+};
