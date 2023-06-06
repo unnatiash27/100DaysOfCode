91997ec2f2c6ec8db1a0f2d7ccc7744181f80729
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-06 08:26:10 +0530

    Create day27.cpp

---
+++ day27.cpp
@@ -0,0 +1,28 @@
+//Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *t;
+        while(headA != NULL)
+        {
+            t=headB;
+            while(t!=NULL)
+            {
+                if(t==headA)return t;
+                t=t->next;
+            }
+            headA=headA->next;
+
+        }
+        return NULL;
+    }
+};
