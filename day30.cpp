bdac88f94144a337c8d4adf97a8414befab4eab2
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-09 07:15:03 +0530

    Update day30.cpp

--- day30.cpp
+++ day30.cpp
@@ -37,10 +37,6 @@ ListNode *HeadNode;
  * int param_1 = obj->getRandom();
  */
 
-
-
-
-
 //You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 /**
