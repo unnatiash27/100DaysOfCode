42a083266d204bb1fed3f14dd6b6fde0754f8cd7
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-11 20:33:49 +0530

    Update day32.cpp

--- day32.cpp
+++ day32.cpp
@@ -1,7 +1,7 @@
 //A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
 
 /*
-// Definition for a Node.
+// Definition for a Node
 class Node {
 public:
     int val;
