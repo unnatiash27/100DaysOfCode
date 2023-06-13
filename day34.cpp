c69ef23e95632c10aa2bddb1a3717ae366a78af6
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-13 18:06:44 +0530

    Update day34.cpp

--- day34.cpp
+++ day34.cpp
@@ -1,1 +1,23 @@
 //Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null
+
+
+class Solution {
+ public:
+  ListNode* detectCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        slow = head;
+        while (slow != fast) {
+          slow = slow->next;
+          fast = fast->next;
+        }
+        return slow;
+      }
+    }
+    return nullptr;
+  }
+};
