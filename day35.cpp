243dc72f29b268c13f7e3ecaf08b5b9937241d0f
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-14 08:30:18 +0530

    Update day35.cpp

--- day35.cpp
+++ day35.cpp
@@ -24,3 +24,21 @@ public:
         return dig[0]*10+dig[2] +  dig[1]*10+dig[3];
     }
 };
+
+
+//Given an integer n, the length of the array, return the minimum number of operations needed to make all the elements of arr equal.
+
+class Solution {
+public:
+    int minOperations(int n) {
+        int min=1;
+        int max=2*(n-1)+1;
+        int eq=(min+max)/2;
+        int ans=0;
+        for(int i=0;i<n/2;i++)
+        {
+            ans+=eq-(2*i+1);
+        }
+        return ans;
+    }
+};
