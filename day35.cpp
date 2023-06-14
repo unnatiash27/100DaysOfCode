e424cf96ae98b4069282501a8844a5e9cb0d95d5
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-14 12:28:33 +0530

    Update day35.cpp

--- day35.cpp
+++ day35.cpp
@@ -42,3 +42,21 @@ public:
         return ans;
     }
 };
+
+//Given an integer num, return the number of steps to reduce it to zero.
+//In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int i;
+        for( i=0;num>0;i++)
+        {
+            if(num%2==0)
+            num=num/2;
+            else
+            num--;
+        }
+        return i;
+    }
+};
