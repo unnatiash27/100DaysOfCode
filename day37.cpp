9560a5d02b40bd3886412a4886823988089cef05
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-16 18:51:32 +0530

    Create day37.cpp

---
+++ day37.cpp
@@ -0,0 +1,32 @@
+//Given a string s, find the length of the longest substring  without repeating characters.
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length()==0)
+        return 0;   
+        unordered_map<char,int> m;  
+        int i=0,j=0,ans=INT_MIN; 
+        while(j<s.length())   
+        {
+            m[s[j]]++; 
+            if(m.size()==j-i+1) 
+            {
+                ans = max(ans,j-i+1); 
+            }
+            else if(m.size()<j-i+1)  
+            {
+                {
+                    m[s[i]]--;   
+                    if(m[s[i]]==0) 
+                    {
+                        m.erase(s[i]);
+                    }
+                    i++; 
+                }
+            }
+             j++; 
+        }
+        return ans;
+    }
+};
