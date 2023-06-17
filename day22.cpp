dd631574d5d9f2cef7f3ae48e390542888e259a4
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-17 19:46:29 +0530

    Update day22.cpp

--- day22.cpp
+++ day22.cpp
@@ -1,7 +1,7 @@
 // Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 
 class Solution
-{
+{ 
 public: 
     int subtractProductAndSum(int n)
     {
