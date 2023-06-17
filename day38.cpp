9fca3b2b12e45ab1cd990533b84fe459c436a4ca
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-17 20:31:46 +0530

    Update day38.cpp

--- day38.cpp
+++ day38.cpp
@@ -39,3 +39,38 @@ public:
         return ans;
     }
 };
+
+
+
+
+//Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+    ListNode* dummy = new ListNode(0);
+    dummy->next = head;
+    int prefix = 0;
+    unordered_map<int, ListNode*> seen;
+    for (ListNode* i = dummy; i; i = i->next) {
+        seen[prefix += i->val] = i;
+    }
+    prefix = 0;
+    for (ListNode* i = dummy; i; i = i->next) {
+        i->next = seen[prefix += i->val]->next;
+    }
+    return dummy->next;
+}
+};
