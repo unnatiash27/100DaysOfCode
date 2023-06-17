35e0f5fc8b367dca2ad7e474938cca535a633f36
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-17 19:45:38 +0530

    Create day38.cpp

---
+++ day38.cpp
@@ -0,0 +1,41 @@
+//Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> ans(k, nullptr);
+        int tlen = 0;
+        ListNode* l = head;
+        while (l != NULL) {
+            tlen++;
+            l = l->next;
+        }
+        int len = tlen / k;
+        int dlen = tlen % k;
+        ListNode* curr = head;
+        ListNode* p = NULL;
+        int i = 0;
+        while (curr != NULL && i < k) {
+            ans[i] = curr;
+            for (int j = 0; j < len + (dlen > 0 ? 1 : 0); j++) {
+                p = curr;
+                curr = curr->next;
+            }
+            p->next = NULL;
+            i++;
+            dlen--;
+        }
+        return ans;
+    }
+};
