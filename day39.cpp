63883a5c043080e00336137845f1b4f786018975
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-18 23:53:06 +0530

    Update day39.cpp

--- day39.cpp
+++ day39.cpp
@@ -1,6 +1,5 @@
 //You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
 
-
 class BrowserHistory {
 public:
 
