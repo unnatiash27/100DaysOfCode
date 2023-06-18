f40cfcc21f6a3f5eb52b371044928628f69c4193
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-18 23:52:46 +0530

    Update day39.cpp

--- day39.cpp
+++ day39.cpp
@@ -77,3 +77,28 @@ public:
         return maxval;
     }
 };
+
+
+
+//Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int n = num, i = 0, j = -1;
+        
+        while (n) {
+            if (n%10 == 6) {
+                j = i;
+            }
+            i++;
+            n/=10;
+        }
+        
+        if (j == -1)
+            return num;
+        
+        return num + pow(10, j)*3;
+    }
+};
