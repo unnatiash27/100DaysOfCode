2cdb9b1722922d282941782d9f0cff0d3b050fe4
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-19 22:42:42 +0530

    Update day40.cpp

--- day40.cpp
+++ day40.cpp
@@ -68,3 +68,51 @@ public:
         return ans;
     }
 };
+
+
+//You are given the head of a singly linked-list. The list can be represented as:
+//L0 → L1 → … → Ln - 1 → Ln
+//Reorder the list to be on the following form:
+//L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // base case : linkedlist is empty
+        if (!head) return;
+        
+        // finding the middle with the help of two pointer approach
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while (tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
+        }
+        
+        // adding one bit in case of lists with even length
+        if (tmp->next) half = half->next;
+        
+        // Now reverse the second half
+        while (half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
+        
+        // After reversing the second half, let's merge both the halfes
+        while (head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
+        }
+        
+        // Base case : closing when we had even length arrays
+        if (head && head->next) head->next->next = NULL;
+    }
+};
