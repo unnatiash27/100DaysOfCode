ce4c8d85dbff460aa7752d89030a5a0ea286ea0e
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-21 14:46:03 +0530

    Update day42.cpp

--- day42.cpp
+++ day42.cpp
@@ -20,3 +20,27 @@ public:
      return m;
     }
 };
+
+
+//You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int>v;
+        for(int i=0;i<prices.size();i++)
+        {
+            v.push_back(prices[i]);
+            for(int j=i+1;j<prices.size();j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    v[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
