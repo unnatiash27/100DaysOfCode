069162af0e6ac0e7aa0548f19662378c9f0161c6
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-21 20:51:58 +0530

    Update day42.cpp

--- day42.cpp
+++ day42.cpp
@@ -85,3 +85,39 @@ public:
         return ans;
     }
 };
+
+
+//Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(st.top() == s[i])
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+        }
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
