15d809ca8f3f69b42ea7df9163abb78b2982b96e
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-21 20:31:31 +0530

    Update day42.cpp

--- day42.cpp
+++ day42.cpp
@@ -44,3 +44,44 @@ public:
         return v;
     }
 };
+
+
+//You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        int ans=0;
+        int x;
+        stack<int>st;
+        for(int i=0;i<operations.size();i++)
+        {
+            if(operations[i] == "+"){
+                int x = st.top();
+                st.pop();
+                int y = st.top();
+                st.pop();
+                
+                st.push(y);
+                st.push(x);
+                st.push(x+y);
+            }
+            else if(operations[i] == "C"){
+                st.pop();
+            }
+            else if(operations[i] == "D"){
+                st.push(2*st.top());
+            }
+            else{
+                int x = stoi(operations[i]);
+                st.push(x);
+            }
+        }
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();       
+        }
+        return ans;
+    }
+};
