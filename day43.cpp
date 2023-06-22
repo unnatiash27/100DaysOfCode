41000c53301ce70332e0befa4fc326708cdb5cd4
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-22 08:21:51 +0530

    Update day43.cpp

--- day43.cpp
+++ day43.cpp
@@ -30,3 +30,27 @@ public:
         return st.size();
     }
 };
+
+
+//Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string>st;
+        for(int i=0;i<logs.size();i++)
+        {
+            if(logs[i]=="../")
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+            }
+            
+                else if(logs[i]!="./")
+                st.push("+");
+        }
+        return st.size();
+    }
+};
