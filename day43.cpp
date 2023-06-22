11c9ab0911a9bdaac97e151fa7754a290e086015
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-22 19:40:52 +0530

    Update day43.cpp

--- day43.cpp
+++ day43.cpp
@@ -174,3 +174,37 @@ int n;
  * int param_2 = obj->pop();
  * obj->increment(k,val);
  */
+
+
+//Return the minimum number of moves required to make s valid.
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int c=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==')')
+            {
+                if(st.empty())
+                {
+                    c++;
+                }
+                else if(st.top()=='(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[i]);
+                }
+            }
+            else if(s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+        }
+        return st.size()+c;
+    }
+};
