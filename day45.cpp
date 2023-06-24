33d0bbcd70a4ea463aa5b04929036bfbffc9473c
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-24 08:31:29 +0530

    Update day45.cpp

--- day45.cpp
+++ day45.cpp
@@ -33,3 +33,34 @@ public:
      return ans;
     }
 };
+
+
+//Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them.
+
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) 
+    {
+        vector<string> ans;
+        int j=0;
+        for(int i=1; i<=n; i++)
+        {
+            if(j==target.size())
+            {
+                break;
+            }
+            if(target[j]==i)
+            {
+                ans.push_back("Push");
+                j++;
+            }
+            else
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+        }
+        return ans;
+    }
+};
