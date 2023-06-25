45f6319c2ba35d35628651c3b7609886d25c0844
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-25 20:21:54 +0530

    Update day46.cpp

--- day46.cpp
+++ day46.cpp
@@ -29,3 +29,44 @@ public:
         return ans;
     }
 };
+
+
+//Given a string s of '(' , ')' and lowercase English characters.
+//Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+class Solution
+{
+public:
+    string minRemoveToMakeValid(string s)
+    {
+        stack<int> st;
+        string res = "";
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == '(')
+                st.push(i);
+            else if (s[i] == ')')
+            {
+                if (st.empty())
+                    s[i] = '#';
+                else
+                    st.pop();
+            }
+        }
+
+        while (!st.empty())
+        {
+            s[st.top()] = '#';
+            st.pop();
+        }
+
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] != '#')
+                res.push_back(s[i]);
+        }
+
+        return res;
+    }
+};
