97da4a0250035cc7baebcff30abcdf9367973b6f
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-26 22:37:49 +0530

    Update day47.cpp

--- day47.cpp
+++ day47.cpp
@@ -23,3 +23,25 @@ public:
         return c;
     }
 };
+
+
+//Given a string s, determine if it is valid.
+//A string s is valid if, starting with an empty string t = "", you can transform t into s after performing the following operation any number of times:
+//Insert string "abc" into any position in t. More formally, t becomes tleft + "abc" + tright, where t == tleft + tright. Note that tleft and tright may be empty.
+//Return true if s is a valid string, otherwise, return false.
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == 'c') {
+                if (st.size() < 2 || st.top() != 'b') return false;
+                st.pop();
+                if (st.top() != 'a') return false;
+                st.pop();
+            } else st.push(c);
+        }
+        return st.empty();
+    }
+};
