db4f47f3c0d4822203eadd69bf559553194f5a2b
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-29 19:21:06 +0530

    Create day50.cpp

---
+++ day50.cpp
@@ -0,0 +1,47 @@
+//You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+
+class Solution {
+public:
+
+    string decodeString(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ']') {
+                st.push(s[i]);
+            }
+            else{
+                string curr_str = "";
+                
+                while(st.top() != '['){
+                    curr_str = st.top() + curr_str ;
+                    st.pop();
+                }
+                
+                st.pop();   // for '['
+                string number = "";
+                
+                // for calculating number
+                
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+                int k_time = stoi(number);    // convert string to number
+                
+                while(k_time--){
+                    for(int p = 0; p < curr_str.size() ; p++)
+                        st.push(curr_str[p]);
+                }
+            }
+        }
+        
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
+        
+    }
+};
