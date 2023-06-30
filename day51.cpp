9702c2841cdc747f7af7f3ec269812e356c00b41
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-06-30 23:14:11 +0530

    Update day51.cpp

--- day51.cpp
+++ day51.cpp
@@ -49,3 +49,25 @@ public:
         return ans;
     }
 };
+
+
+//Return the sum of all subarray ranges of nums.
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            int mini=INT_MAX;
+            int maxi=INT_MIN;
+            for(int j=i;j<nums.size();j++)
+            {
+                mini=min(mini,nums[j]);
+                maxi=max(maxi,nums[j]);
+                ans+=maxi-mini;
+            }
+        }
+        return ans;
+    }
+};
