1ae0e96a2cb684c0077315f8569c647c2ec0421f
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-04 19:15:58 +0530

    Create day55.cpp

---
+++ day55.cpp
@@ -0,0 +1,10 @@
+//Return the number of matches played in the tournament until a winner is decided.
+
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+        
+    }
+};
