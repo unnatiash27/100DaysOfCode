56dd1a1afd4c32c04bc65b8264f89852f0530836
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-05 19:39:35 +0530

    Update day56.cpp

--- day56.cpp
+++ day56.cpp
@@ -2,7 +2,7 @@
 //Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
 //Return the bitwise XOR of all elements of nums.
 
-class Solution {
+class Solution {  
 public:
     int xorOperation(int n, int start) {
         int nums[n];
