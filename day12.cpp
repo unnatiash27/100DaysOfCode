29bb66fefe85cf796fad0927ba4320004e6c999b
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-05 19:41:00 +0530

    Update day12.cpp

--- day12.cpp
+++ day12.cpp
@@ -1,5 +1,5 @@
 // Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
-  
+    
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
