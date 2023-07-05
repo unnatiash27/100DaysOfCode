9c370b27795c90bb1495d7d9dd3ef2f32816ab3a
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-05 19:39:58 +0530

    Update day56.cpp

--- day56.cpp
+++ day56.cpp
@@ -1,6 +1,6 @@
 //You are given an integer n and an integer start.
 //Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
-//Return the bitwise XOR of all elements of nums.
+//Return the bitwise XOR of all elements of nums.  
 
 class Solution {    
 public:  
