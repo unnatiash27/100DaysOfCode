570a32d5503beba42a7a3459cb0bf71928c4af60
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-07 19:24:39 +0530

    Update day10.cpp

--- day10.cpp
+++ day10.cpp
@@ -1,7 +1,7 @@
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
 //The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
-class Solution {
+class Solution {  
 public:
     int removeDuplicates(vector<int>& nums) {
         int x=0;
