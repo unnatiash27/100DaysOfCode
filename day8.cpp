deb6eb24307dd198e42dc37425a6b762c7c95e01
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-08 18:32:59 +0530

    Update day8.cpp

--- day8.cpp
+++ day8.cpp
@@ -17,7 +17,7 @@ public:
      return ans;
     }
 };
-
+  
 
 //You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
 
