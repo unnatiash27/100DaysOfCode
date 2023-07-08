5bccc8c13c95956b13e7addbf4a7f9b10f96fe08
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-08 18:32:52 +0530

    Update day8.cpp

--- day8.cpp
+++ day8.cpp
@@ -1,7 +1,7 @@
  //You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
 //There is a function shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
    
-
+   
 class Solution {
 public:
     string replaceDigits(string s) 
