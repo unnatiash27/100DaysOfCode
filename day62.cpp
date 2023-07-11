9bf65a190089bbc9cdd27eeceec70f39e42f66d8
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-11 20:46:03 +0530

    Create day62.cpp

---
+++ day62.cpp
@@ -0,0 +1,9 @@
+//Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+         return num + 2*t;
+    }
+};
