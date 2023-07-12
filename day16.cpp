d4ae945b16f00e4c9a132ffaec3ea1a726148148
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-12 21:33:58 +0530

    Update day16.cpp

--- day16.cpp
+++ day16.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     bool checkString(string s) {
-        for(int i=0;i<s.size();i++)
+        for(int i=0;i<s.size();i++) 
         {
             if(s[i]=='b' && s[i+1]=='a'){
             return false;} 
