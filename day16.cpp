2d39a9ee42ee948b3e646d17fc24adfceb047619
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-12 21:34:44 +0530

    Update day16.cpp

--- day16.cpp
+++ day16.cpp
@@ -1,5 +1,5 @@
 //Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
- 
+
 class Solution {
 public:
     bool checkString(string s) {
