b7224f681f1ac7409efc24879edc9571c256485e
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-13 20:27:40 +0530

    Update day9.cpp

--- day9.cpp
+++ day9.cpp
@@ -25,7 +25,7 @@ public:
  
 //Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
 
-class Solution {
+class Solution { 
 public:
     int numOfStrings(vector<string>& patterns, string word) {
         int count=0;
