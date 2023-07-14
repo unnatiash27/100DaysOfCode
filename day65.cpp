d74f69b835150afb8e90cfb96c5dfe0c4f3e1ce2
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-14 21:49:09 +0530

    Create day65.cpp

---
+++ day65.cpp
@@ -0,0 +1,14 @@
+//Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+
+class Solution {
+public:
+     int findGCD(vector<int>& nums) {
+        int maxi=INT_MIN,mini=INT_MAX;
+         for(int i=0;i<nums.size();i++){
+           maxi=max(maxi,nums[i]);
+           mini=min(mini,nums[i]);
+         }
+         return gcd(maxi,mini);
+    }
+};
