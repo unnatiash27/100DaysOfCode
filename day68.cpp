0366461c73f564b131a0544b9ffcc3c27126b31e
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-17 21:48:47 +0530

    Update day68.cpp

--- day68.cpp
+++ day68.cpp
@@ -1,2 +1,18 @@
 //Return an array (of length num_people and sum candies) that represents the final distribution of candies.
 
+
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int npeople) {
+        vector<int>ans(npeople,0);
+        int i=0,n=1;
+        while(candies>0){
+            i = i%npeople;
+            ans[i]= ans[i]+min(candies,n);
+            candies-=n;
+            n++;
+            i++;
+        }
+        return ans;
+    }
+};
