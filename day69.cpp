5cbd2e7bdb78734eeea258811c3a6d2c0e3de563
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-18 20:32:59 +0530

    Update day69.cpp

--- day69.cpp
+++ day69.cpp
@@ -21,7 +21,7 @@ public:
     }
 };
   
-/**
+/**  
  * Your RecentCounter object will be instantiated and called as such:
  * RecentCounter* obj = new RecentCounter();
  * int param_1 = obj->ping(t);
