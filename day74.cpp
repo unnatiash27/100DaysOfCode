7ae87984d8764642774225bf071cc81f4c9547cc
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-23 20:14:36 +0530

    Update day74.cpp

--- day74.cpp
+++ day74.cpp
@@ -5,7 +5,7 @@ public:
     int mod=1e9+7;
     int peopleAwareOfSecret(int n, int delay, int forget) {
        vector<int> curr(2001,0);
-       curr[1]=1;
+       curr[1]=1;  
        for(int i=1;i<=n;i++)
        {
            for(int j=i+delay;j<i+forget;j++)
