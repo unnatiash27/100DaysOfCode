5f215242ddaa146b539d37ff5b603a540cf0f59a
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-24 22:14:32 +0530

    Update day75.cpp

--- day75.cpp
+++ day75.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();++i)
         {
            if(nums[i]<minK || nums[i]>maxK)
-           {
+           {   
                mini=-1;
                maxi=-1;
                l=i;
