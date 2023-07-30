27f83eadc8df4efbe75ee90f5c763a355d2b8451
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-30 11:21:48 +0530

    Create day81.cpp

---
+++ day81.cpp
@@ -0,0 +1,31 @@
+//You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        for(auto &i:tokens)
+        {
+            if(i == "+" || i == "-"  || i == "*"  || i == "/")
+            {
+                long long  x = st.top() ; 
+                st.pop() ;
+                long long  y = st.top() ; 
+                st.pop() ;
+
+                if(i=="+")
+                x=x+y;
+                if(i=="-")
+                x=y-x;
+                if(i=="*")
+                x=x*y;
+                if(i=="/")
+                x=y/x;
+                st.push(x);
+            }
+            else
+            st.push(stoi(i));
+        }
+        return st.top();
+    }
+};
