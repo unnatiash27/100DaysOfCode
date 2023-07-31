712512d0b6903fe94b3500210555f338d697a3d9
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-07-31 19:36:10 +0530

    Create day82.cpp

---
+++ day82.cpp
@@ -0,0 +1,41 @@
+//Given an integer array nums, find a 
+//subarray
+// that has the largest product, and return the product.
+
+/*class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            int p=1;
+            for(int j=i;j<nums.size();j++)
+            {
+                p=p*nums[j];
+                if(p>max)
+                {
+                    max=p;
+                }
+            }
+        }
+        return max;
+    }
+};*/
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = nums[0];
+        int maxSoFar = ans, minSoFar = ans;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+                swap(maxSoFar,minSoFar);
+
+            maxSoFar = max(maxSoFar*nums[i],nums[i]);
+            minSoFar = min(minSoFar*nums[i],nums[i]);
+
+            ans = max(maxSoFar,ans);
+        }
+        return ans;
+    }
+};
