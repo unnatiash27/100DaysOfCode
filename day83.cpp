91ec68c6038f861a50b812c510f2c29c1577511d
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-01 19:17:49 +0530

    Update day83.cpp

--- day83.cpp
+++ day83.cpp
@@ -1,1 +1,23 @@
-//
+//Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root==NULL)
+        return false;
+        if(root->left->val + root->right->val == root->val)
+        return true;
+        return false;
+    }
+};
