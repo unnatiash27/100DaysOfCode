6ac5e9331b3ce649c0528c75862a727e68d3a26b
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-01 19:18:31 +0530

    Update day83.cpp

--- day83.cpp
+++ day83.cpp
@@ -10,7 +10,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ */  
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
