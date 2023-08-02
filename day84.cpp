3e86659e944e806c181d046d02637c951ea42b24
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-02 19:54:05 +0530

    Update day84.cpp

--- day84.cpp
+++ day84.cpp
@@ -1,2 +1,16 @@
 //Given a string s, reverse only all the vowels in the string and return it.
 
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<int>v;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            v.push_back(i);
+        }
+        for(int i=0;i<v.size()/2;i++)
+        swap(s[v[i]],s[v[v.size()-1-i]]);
+        return s;
+    }
+};
