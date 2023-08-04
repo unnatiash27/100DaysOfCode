99c59fff95f731d135e9dc41653952d5114e2e15
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-04 11:09:18 +0530

    Update day86.cpp

--- day86.cpp
+++ day86.cpp
@@ -1,1 +1,32 @@
 //Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        int diff=INT_MAX;
+        int p=-1;
+        inorder(root,diff,p);
+        return diff;
+    }
+    void inorder(TreeNode* root, int& diff,int& p)
+    {
+        if(root==NULL)return;
+        inorder(root->left,diff,p);
+        if(p!=-1)
+        {
+            diff=min(diff,root->val-p);
+        }
+        p=root->val;
+        inorder(root->right,diff,p);
+    }
+};
