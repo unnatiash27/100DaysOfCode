9a9af733a8d07272b88c8edbf8ad7c3b586f8530
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-04 11:10:04 +0530

    Update day86.cpp

--- day86.cpp
+++ day86.cpp
@@ -11,7 +11,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };  
- */
+ */   
 class Solution {  
 public:
     int minDiffInBST(TreeNode* root) {
