70f3cbcd2bed1777b6c9eb9d3a9d3f67da7cfc6b
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-06 22:45:05 +0530

    Update day88.cpp

--- day88.cpp
+++ day88.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        vector<int> ch(26,0);
+        vector<int> ch(26,0);  
 		// store the broken letters in ch vector
         for(char c: brokenLetters){
             ch[c-'a']=1;
