9f92d68de2527e3f869b8c4fecfbc0d2e9e83b85
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-09 15:14:34 +0530

    Update day 91.cpp

--- day 91.cpp
+++ day 91.cpp
@@ -1,1 +1,24 @@
-//Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return nullptr;
+
+            if(p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left,p,q);
+
+            if(p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right,p,q);
+
+        return root;
+    }
+};//Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
