9a8e93e719ededa9b5089490d26fc169b64edd85
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-11 14:01:35 +0530

    Update day93.cpp

--- day93.cpp
+++ day93.cpp
@@ -8,7 +8,7 @@ public:
         dp[1]=1;
         for(int i=2;i<=n;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=i;j++)   
             dp[i]+=dp[j-1]*dp[i-j];   
         }
         return dp[n];  
