ad0a07f7a989a41f85821e19c25da643d7f85a8c
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-11 14:00:25 +0530

    Update day93.cpp

--- day93.cpp
+++ day93.cpp
@@ -1,1 +1,17 @@
 //Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            dp[i]+=dp[j-1]*dp[i-j];
+        }
+        return dp[n];
+    }
+    
+};
