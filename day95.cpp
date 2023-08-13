34d39bc8081d9fbae85b928b23fbbd1214ae6b9f
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-13 10:57:24 +0530

    Update day95.cpp

--- day95.cpp
+++ day95.cpp
@@ -1,2 +1,37 @@
 //Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+        vector<int>v;
+    vector<int> postorder(Node* root) {
+
+        if(root == NULL)
+        {vector<int>vi;
+            return vi;}
+        for(auto n:root->children)
+        {postorder(n);}
+        v.push_back(root->val);
+
+        return v;
+    }
+};
