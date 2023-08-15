9e49eae6774a019f528f17973bb78a8fda5e5372
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-15 08:48:15 +0530

    Update day97.cpp

--- day97.cpp
+++ day97.cpp
@@ -1,1 +1,20 @@
 //Return an array of strings containing the new strings formed after the splits, excluding empty strings.
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+        vector<string> ans;
+        for(int i=0; i<words.size();i++){
+            string s = words[i];
+            string temp ="";
+            for(auto a:s){
+                if(a==separator && temp!=""){
+                    ans.push_back(temp);
+                    temp="";
+                }
+                if(a!=separator) temp+=a;
+            }
+            if(temp!="")ans.push_back(temp);
+        }
+        return ans;
+    }
+};
