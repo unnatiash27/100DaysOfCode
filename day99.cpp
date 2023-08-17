35c365248220f7235545ac77ea074c4500e71c7a
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-17 10:38:18 +0530

    Update day99.cpp

--- day99.cpp
+++ day99.cpp
@@ -7,7 +7,7 @@ public:
      for(int i=0;i<arr.size()-2;i++)
      {
          
-         for(int j=i+1;j<arr.size()-1;j++)
+         for(int j=i+1;j<arr.size()-1;j++) 
          {
              if( (abs(arr[i]-arr[j])<=a) ){
              for(int k=j+1;k<arr.size();k++)
