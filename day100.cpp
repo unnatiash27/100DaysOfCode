30937faaf6ce3bfd48dd8bf19aa0aa4cad879f44
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-19 20:02:48 +0530

    Update day100.cpp

--- day100.cpp
+++ day100.cpp
@@ -25,3 +25,23 @@ public:
     }
 
 };
+
+
+
+//2778. Sum of Squares of Special Elements
+
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int sum=0;
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(nums.size() % i == 0)
+            {
+                int sq=nums[i-1]*nums[i-1];
+                sum+=sq;
+            }
+        }
+        return sum;
+    }
+};
