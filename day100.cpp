d77269e1135f22614d0a37af3e50b58cee42d627
unnatiash27 <113242914+unnatiash27@users.noreply.github.com>
2023-08-19 20:04:32 +0530

    Update day100.cpp

--- day100.cpp
+++ day100.cpp
@@ -8,7 +8,7 @@ public:
         vector<vector<int>> answer(2);
         for (auto num : nums1)
             s1.insert(num);
-        for (auto num : nums2)
+        for (auto num : nums2)   
             s2.insert(num);
         for (auto num : s1) { 
             if (s2.find(num) == s2.end()) 
